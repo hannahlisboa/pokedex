572356d501c8538bb2e55aad6163ce3f2262d8ad
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-19 20:49:02 -0300

    add extension to break array

---
+++ pokedex-challenge/pokedex-challenge/Extensions/Array+Chunk.swift
@@ -0,0 +1,25 @@
+//
+//  Array+Chunk.swift
+//  pokedex-challenge
+//
+//  Created by Hannah  on 19/05/2020.
+//  Copyright © 2020 Hannah . All rights reserved.
+//
+import Foundation
+
+extension Array {
+    func chunked(into size:Int) -> [[PokemonListItem]] {
+        
+        var chunkedArray = [[PokemonListItem]]()
+        
+        for index in 0...self.count {
+            if index % size == 0 && index != 0 {
+                chunkedArray.append(Array(self[(index - size)..<index]) as! [PokemonListItem])
+            } else if(index == self.count && chunkedArray.count>=0) {
+                chunkedArray.append(Array(self[(index>0 ? index - 1 : index)..<index]) as! [PokemonListItem])
+            }
+        }
+        return chunkedArray
+    }
+}
+
