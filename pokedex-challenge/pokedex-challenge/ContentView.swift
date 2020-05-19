dc7f5d13902f05e2061901066cba0df210f36b07
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-19 13:55:05 -0300

    create project

---
+++ pokedex-challenge/pokedex-challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pokedex-challenge
+//
+//  Created by Hannah  on 19/05/2020.
+//  Copyright © 2020 Hannah . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
