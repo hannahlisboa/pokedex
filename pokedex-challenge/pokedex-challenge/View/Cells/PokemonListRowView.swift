22ef2918ce22b0a7e9ba600f80f2007a1ad84ea9
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-19 20:52:44 -0300

    Create Row to grid

---
+++ pokedex-challenge/pokedex-challenge/View/Cells/PokemonListRowView.swift
@@ -0,0 +1,39 @@
+//
+//  PokemonListRowView.swift
+//  pokedex-challenge
+//
+//  Created by Hannah  on 19/05/2020.
+//  Copyright © 2020 Hannah . All rights reserved.
+//
+
+import SwiftUI
+
+struct PokemonListRowView:  View {
+    
+    var pokemonListRow: [PokemonListItem]
+    
+    var body: some View {
+        HStack(alignment: .center) {
+            Spacer()
+            
+            ForEach(self.pokemonListRow, id: \.self) { pokemonItem in
+                VStack{
+//                    MoviesListCellView(movie: movie)
+                    PokemonListCellView(pokemonItem: pokemonItem)
+                }
+                .padding(.bottom, 8)
+            }
+            Spacer()
+            
+        }
+    }
+    
+}
+//struct PokemonListRowView_Previews: PreviewProvider {
+//    
+//    @State static var pokemonRow = [PokemonListRowView]()
+//    
+//    static var previews: some View {
+//        PokemonListRowView(pokemonListRow: pokemonRow)
+//    }
+//}
