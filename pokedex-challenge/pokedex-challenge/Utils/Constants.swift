688cf2d1b147e5bf0993d8b6f76338f7e172169c
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-19 15:45:11 -0300

    Create Constants file and add const to api

---
+++ pokedex-challenge/pokedex-challenge/Utils/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  pokedex-challenge
+//
+//  Created by Hannah  on 19/05/2020.
+//  Copyright © 2020 Hannah . All rights reserved.
+//
+
+import Foundation
+
+
+struct Constants {
+    
+    struct API {
+        static let pokeApiUrl = "https://pokeapi.co/api/v2/"
+    }
+}
