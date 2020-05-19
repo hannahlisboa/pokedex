55fd9a9b521ea06dacae1047099cf3f5a8c53972
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-19 20:06:30 -0300

    add root navigation

---
+++ pokedex-challenge/pokedex-challenge/View/RootNavigationView.swift
@@ -0,0 +1,49 @@
+//
+//  RootView.swift
+//  pokedex-challenge
+//
+//  Created by Hannah  on 19/05/2020.
+//  Copyright © 2020 Hannah . All rights reserved.
+//
+
+import SwiftUI
+
+struct RootNavigationView: View {
+    
+    init() {
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(named: Constants.Design.Color.Yellow)
+
+        let fontLarge: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor(named: Constants.Design.Color.Blue)!,
+            .font: UIFont(name:Constants.Design.Font.BrandFont, size: 40)!
+        ]
+        
+        let fontNormal: [NSAttributedString.Key: Any] = [
+                 .foregroundColor: UIColor(named: Constants.Design.Color.Blue)!,
+                 .font: UIFont(name:Constants.Design.Font.BrandFont, size: 25)!
+             ]
+
+        appearance.largeTitleTextAttributes = fontLarge
+        appearance.titleTextAttributes = fontNormal
+        
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+     }
+    
+    
+    var body: some View {
+        NavigationView{
+            PokemonListView()
+            .navigationBarTitle("Pokédex")
+        }
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootNavigationView()
+    }
+}
