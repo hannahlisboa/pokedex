2cb0fa1bac2d1be21ba35ea832d038c5c521d96d
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-20 02:18:06 -0300

    Create PokemonDetailView.swift

---
+++ pokedex-challenge/pokedex-challenge/View/PokemonDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  PokemonDetailView.swift
+//  pokedex-challenge
+//
+//  Created by Hannah  on 20/05/2020.
+//  Copyright © 2020 Hannah . All rights reserved.
+//
+
+import SwiftUI
+
+struct PokemonDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokemonDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonDetailView()
+    }
+}
