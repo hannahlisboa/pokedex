72da7ef459b037466deca5f3c8a1d90734d44471
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-20 13:45:00 -0300

    add button for load detail view in Pokemon List Cell

--- pokedex-challenge/pokedex-challenge/View/Cells/PokemonListCellView.swift
+++ pokedex-challenge/pokedex-challenge/View/Cells/PokemonListCellView.swift
@@ -11,30 +11,40 @@ import SwiftUI
 struct PokemonListCellView: View {
     
     @ObservedObject var pokeListCellVM: PokemonListCellViewModel
+    @State var showSheetView = false
     
     init(pokemonItem: PokemonListItem) {
         self.pokeListCellVM = PokemonListCellViewModel(pokemonItem: pokemonItem)
     }
     var body: some View {
         ZStack(alignment: .topTrailing){
-            VStack(alignment: .center){
-                ImageViewComponent(url: pokeListCellVM.urlImage, type: .gridCell)
-                HStack{
-                    Text("\(pokeListCellVM.id)")
-                    .textStyle(IdCellStyle())                    
-                    Text(pokeListCellVM.name)
-                    .textStyle(TitleCellStyle())
-                }.padding(.bottom, 5)
-            }
+            Button(action: {
+                self.showSheetView.toggle()
+                
+            }, label: {
+                VStack(alignment: .center){
+                    ImageViewComponent(url: pokeListCellVM.urlImage, type: .gridCell)
+                    HStack{
+                        Text("\(pokeListCellVM.id)")
+                            .textStyle(IdCellStyle())
+                        Text(pokeListCellVM.name)
+                            .textStyle(TitleCellStyle())
+                    }.padding(.bottom, 5)
+                }
+            }).buttonStyle(ButtonAnimatedStyle())
         }.padding()
             .background(Color.white)
             .cornerRadius(15)
             .shadow(color: Color.black.opacity(0.2), radius: 7, x: 0, y: 1)
+            .sheet(isPresented: $showSheetView) {
+                PokemonDetailView(id: self.pokeListCellVM.id)
+        }
     }
 }
 
-//struct PokemonListCellView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        PokemonListCellView()
-//    }
-//}
+struct SheetView: View {
+    var body: some View {
+        Text("Sheet View")
+    }
+}
+
