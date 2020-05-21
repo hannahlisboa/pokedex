d8a08ee9931140272e2ffb5dea7fb94aefeab422
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-21 02:33:14 -0300

    add text styles

--- pokedex-challenge/pokedex-challenge/Styles/TextStyles.swift
+++ pokedex-challenge/pokedex-challenge/Styles/TextStyles.swift
@@ -10,15 +10,38 @@ import SwiftUI
 struct TitleCellStyle: ViewModifier {
     func body(content: Content) -> some View {
         content
-           .font(Font.custom(Constants.Design.Font.Title, size: 16))
-           .foregroundColor(Color(Constants.Design.Color.Blue))
+            .font(Font.custom(Constants.Design.Font.Title, size: 16))
+            .foregroundColor(Color(Constants.Design.Color.Blue))
+    }
+}
+
+struct PokemonNameDetailStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(Font.custom(Constants.Design.Font.SpecialItems, size: 26))
+            .foregroundColor(Color.white)
+    }
+}
+struct PokemonInfoDetailStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 12))
+            .foregroundColor(Color.white)
+    }
+}
+
+struct DetailTitleStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(Font.custom(Constants.Design.Font.BrandFont, size: 20))
+            .foregroundColor(Color(Constants.Design.Color.Blue))
     }
 }
 struct IdCellStyle: ViewModifier {
     func body(content: Content) -> some View {
         content
-         .font(Font.custom(Constants.Design.Font.SpecialItems, size: 14))
-         .foregroundColor(Color(Constants.Design.Color.Blue))
+            .font(Font.custom(Constants.Design.Font.SpecialItems, size: 14))
+            .foregroundColor(Color(Constants.Design.Color.Blue))
     }
 }
 
