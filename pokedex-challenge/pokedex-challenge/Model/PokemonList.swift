f771358b56cd7a901596bbef935eb974247697ed
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-20 22:51:32 -0300

    add id to item model

--- pokedex-challenge/pokedex-challenge/Model/PokemonList.swift
+++ pokedex-challenge/pokedex-challenge/Model/PokemonList.swift
@@ -19,6 +19,13 @@ struct PokemonList:  Hashable, Codable {
 }
 
 struct PokemonListItem: Hashable, Codable {
+    var id: String?
     let name: String
     let url: String
+    
+    init(id: String, name: String, url: String) {
+        self.id = id
+        self.name = name
+        self.url = url
+    }
 }
