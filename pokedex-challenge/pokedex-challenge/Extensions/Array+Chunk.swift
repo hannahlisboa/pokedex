7497ab645485fbd492e425799162aa313aa6bdea
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-22 15:22:05 -0300

    change chunk extension to add generic array

--- pokedex-challenge/pokedex-challenge/Extensions/Array+Chunk.swift
+++ pokedex-challenge/pokedex-challenge/Extensions/Array+Chunk.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 extension Array {
-    func chunked(into size:Int) -> [[PokemonListItem]] {
+    func chunked(into size:Int) -> [[Element]] {
         
-        var chunkedArray = [[PokemonListItem]]()
+        var chunkedArray = [[Element]]()
         
         for index in 0...self.count {
             if index % size == 0 && index != 0 {
-                chunkedArray.append(Array(self[(index - size)..<index]) as! [PokemonListItem])
+                chunkedArray.append(Array(self[(index - size)..<index]) )
             } else if(index == self.count && chunkedArray.count>=0) {
-                chunkedArray.append(Array(self[(index>0 ? index - 1 : index)..<index]) as! [PokemonListItem])
+                chunkedArray.append(Array(self[(index>0 ? index - 1 : index)..<index]) )
             }
         }
         return chunkedArray
