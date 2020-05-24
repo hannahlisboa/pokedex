760babb236465de86fdf48758ace2a0eaa1e7feb
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-24 00:24:43 -0300

    add activity indicator when requesting pokemon

--- pokedex-challenge/pokedex-challenge/View/PokemonDetailView.swift
+++ pokedex-challenge/pokedex-challenge/View/PokemonDetailView.swift
@@ -16,90 +16,94 @@ struct PokemonDetailView: View {
     init(pokeItem: PokemonListItem) {
         self.pokeItem = pokeItem
         pokemonDetailVM = PokemonDetailViewModel(id: pokeItem.id!)
-
+        
     }
     
     var body: some View {
         ZStack {
-            NavigationView {
-                Text("")
-            }
-            .navigationBarTitle("")
-            .navigationBarHidden(true)
-            LinearGradient(gradient: Gradient(colors: [pokemonDetailVM.color, .white]), startPoint: .top, endPoint: .bottom)
-                .edgesIgnoringSafeArea(.all)
-            VStack {
-                HStack{
-                    Spacer()
-                    VStack{
-                        VStack{
-                            Text("# \(pokemonDetailVM.id)")
-                                .textStyle(PokemonNameDetailStyle())
-                            Text(pokemonDetailVM.name.capitalized).textStyle(PokemonNameDetailStyle())
-                                .padding()
-                        }                        .padding(.top, 30)
-                        
-                        HStack(spacing: 20){
-                            ForEach(pokemonDetailVM.types, id: \.self) { typeName in
-                                VStack{
-                                    Text(typeName)
-                                    
-                                }.padding(5)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 5)
-                                            .stroke(Types.Pokemon(rawValue: typeName)!.color, lineWidth: 2.9))
-                                    .background(Color.white)
-                                    
-                                    //                                    .cornerRadius(15)
-                                    .shadow(color: Color.black.opacity(0.2), radius: 7, x: 10, y: 10)
-                                    .foregroundColor(Types.Pokemon(rawValue: typeName)!.color)
-                            }
-                        }.padding(.bottom)
-                    }
-                    VStack{
-                        ImageViewComponent(url: pokemonDetailVM.urlImage, type: .banner)
-                    }
-                    .padding(.trailing, 20)
-                    .padding(.leading, 20)
-                }.padding(.top, 30)
-                HStack{
-                    Spacer()
-                    Text("Height: \(pokemonDetailVM.height)m").textStyle(PokemonInfoDetailStyle())
-                    Text("Weight: \(pokemonDetailVM.weight)kg")
-                        .textStyle(PokemonInfoDetailStyle())
-                }.padding(.trailing, 20)
-                
-                VStack{
+            LoadingView(isShowing: pokemonDetailVM.isLoading, content: {
+                ZStack{
+                NavigationView {
+                    Text("")
+                }
+                .navigationBarTitle("")
+                .navigationBarHidden(true)
+                    LinearGradient(gradient: Gradient(colors: [self.pokemonDetailVM.color, .white]), startPoint: .top, endPoint: .bottom)
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
                     HStack{
                         Spacer()
-                    }
-                    VStack(spacing: 8){
-                        
-                        SegmentedComponent(selected: self.$selected, primaryColor: pokemonDetailVM.color)
-                        
-                        if self.selected == 0{
-                            StatsView(pokeStats: pokemonDetailVM.stats, color: pokemonDetailVM.color)
-                        }
-                        else{
+                        VStack{
+                            VStack{
+                                Text("# \(self.pokemonDetailVM.id)")
+                                    .textStyle(PokemonNameDetailStyle())
+                                Text(self.pokemonDetailVM.name.capitalized).textStyle(PokemonNameDetailStyle())
+                                    .padding()
+                            }                        .padding(.top, 30)
                             
-                            EvolutionView(id: self.pokemonDetailVM.id)
+                            HStack(spacing: 20){
+                                ForEach(self.pokemonDetailVM.types, id: \.self) { typeName in
+                                    VStack{
+                                        Text(typeName)
+                                        
+                                    }.padding(5)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 5)
+                                                .stroke(Types.Pokemon(rawValue: typeName)!.color, lineWidth: 2.9))
+                                        .background(Color.white)
+                                        
+                                        //                                    .cornerRadius(15)
+                                        .shadow(color: Color.black.opacity(0.2), radius: 7, x: 10, y: 10)
+                                        .foregroundColor(Types.Pokemon(rawValue: typeName)!.color)
+                                }
+                            }.padding(.bottom)
                         }
-                        
-                    }.background(Color("Color").edgesIgnoringSafeArea(.all))
-                    Spacer()
-                }.background(Color.white)
-                    .cornerRadius(radius: 40, corners: [.topLeft, .topRight])
+                        VStack{
+                            ImageViewComponent(url: self.pokemonDetailVM.urlImage, type: .banner)
+                        }.opacity(self.pokemonDetailVM.urlImage.isEmpty ? 0: 1)
+                        .padding(.trailing, 20)
+                        .padding(.leading, 20)
+                    }.padding(.top, 30)
+                    HStack{
+                        Spacer()
+                        Text("Height: \(self.pokemonDetailVM.height)m").textStyle(PokemonInfoDetailStyle())
+                        Text("Weight: \(self.pokemonDetailVM.weight)kg")
+                            .textStyle(PokemonInfoDetailStyle())
+                    }.padding(.trailing, 20)
                     
-                    .padding(.top, 20)
+                    VStack{
+                        HStack{
+                            Spacer()
+                        }
+                        VStack(spacing: 8){
+                            
+                            SegmentedComponent(selected: self.$selected, primaryColor: self.pokemonDetailVM.color)
+                            
+                            if self.selected == 0{
+                                StatsView(pokeStats: self.pokemonDetailVM.stats, color: self.pokemonDetailVM.color)
+                            }
+                            else{
+                                
+                                EvolutionView(id: self.pokemonDetailVM.id)
+                            }
+                            
+                        }.background(Color("Color").edgesIgnoringSafeArea(.all))
+                        Spacer()
+                    }.background(Color.white)
+                        .cornerRadius(radius: 40, corners: [.topLeft, .topRight])
+                        
+                        .padding(.top, 20)
+                    
+                    
+                }
+                .onAppear(){
+                    self.pokemonDetailVM.fetchPokemon()
+                    
+                }
+                NavigationBackButton(navigationTitle: "", navigationColor: Color.white)
+                }
                 
-                
-            }
-            .onAppear(){
-                self.pokemonDetailVM.fetchPokemon()
-                
-            }
-            NavigationBackButton(navigationTitle: "", navigationColor: Color.white)
-            
+            })
         }
     }
 }
