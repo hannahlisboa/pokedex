12d93a3517100cb48c8b8b56f310a8d3db3b798b
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-25 17:17:39 -0300

    Create Sprit.swift

---
+++ pokedex-challenge/pokedex-challenge/Model/Sprit.swift
@@ -0,0 +1,34 @@
+//
+//  Sprite.swift
+//  pokedex-challenge
+//
+//  Created by Hannah  on 25/05/2020.
+//  Copyright © 2020 Hannah . All rights reserved.
+//
+
+import Foundation
+
+struct Sprit: Codable, Hashable {
+    
+    let backDefault: String?
+    let backFemale: String?
+    let backShiny: String?
+    let backShinyFemale: String?
+    let frontDefault: String?
+    let frontFemale: String?
+    let frontShiny: String?
+    let frontShinyFemale: String?
+
+    enum CodingKeys: String, CodingKey {
+        case backDefault = "back_default"
+        case backFemale = "back_female"
+        case backShiny = "back_shiny"
+        case backShinyFemale = "back_shiny_female"
+        case frontDefault = "front_default"
+        case frontFemale = "front_female"
+        case frontShiny = "front_shiny"
+        case frontShinyFemale = "front_shiny_female"
+
+    }
+    
+}
