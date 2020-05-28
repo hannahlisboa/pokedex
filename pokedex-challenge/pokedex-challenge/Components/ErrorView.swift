35d8d4c437af65752918ecbd86d7d6e687e7d018
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-28 16:13:03 -0300

    Create ErrorView.swift

---
+++ pokedex-challenge/pokedex-challenge/Components/ErrorView.swift
@@ -0,0 +1,35 @@
+//
+//  ErrorView.swift
+//  pokedex-challenge
+//
+//  Created by Hannah  on 26/05/2020.
+//  Copyright © 2020 Hannah . All rights reserved.
+//
+
+import SwiftUI
+
+/// view responsable from show message error
+struct ErrorView: View {
+    var show: Bool
+    var tapView: () -> Void
+    var body: some View {
+        
+        VStack {
+            HStack {
+                  Spacer()
+              }
+            if self.show {
+                Spacer()
+                Text("☹️").font(.system(size: 100))
+                Text("An error has occurred! \n Tap here to try again")
+                    .textStyle(NotFoundStyle())
+                Spacer()
+                
+            }
+        }
+            .onTapGesture {
+                self.tapView()
+        }
+        
+    }
+}
